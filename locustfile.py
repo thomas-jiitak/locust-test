a2af9c998c64ab5228bc18297d7da016f4b6cbc3
thomas-jiitak <thomasvjoseph6@gmail.com>
2024-10-23 09:46:22 +0530

    changes in locust file and ci

--- locustfile.py
+++ locustfile.py
@@ -3,5 +3,6 @@ from locust import HttpUser, task
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
-        self.client.get("/login")
-        self.client.get("/in/LoginHelp")
+        # Adding a User-Agent header to mimic a browser request
+        self.client.get("/login", headers={"User-Agent": "Mozilla/5.0"})
+        self.client.get("/in/LoginHelp", headers={"User-Agent": "Mozilla/5.0"})
